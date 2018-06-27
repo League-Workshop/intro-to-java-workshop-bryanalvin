ee4dc07b015a2aa0372ba92e6e118af985ec5396
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-27 11:52:25 -0700

    Day 3

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,33 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot Seiya= new Robot("batman");
+	Seiya.penDown();
+
+	Seiya.setSpeed(100);
 	
-		
-		
+	for (int i = 0; i < 1000; i++) {
+		Seiya.move(130); 
+		Seiya.turn(90);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
